5756d6a6096477d1666374afa6071c6e27e588d8
peachest <75366539+peachest@users.noreply.github.com>
2023-07-05 21:44:32 +0800

    docs: initialize storybook dev environment

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+
+
+const config = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["..\\public"],
+    webpackFinal: async config => {
+        if (config.resolve) {
+            config.resolve.plugins = [
+                ...(config.resolve.plugins || []),
+                new TsconfigPathsPlugin({
+                    extensions: config.resolve.extensions,
+                }),
+            ];
+        }
+        return config;
+    }
+};
+export default config;
---
+++ src/stories/components/example/Button.tsx
@@ -0,0 +1,63 @@
+import React, {JSX, MouseEventHandler} from "react" ;
+import "./button.css" ;
+
+/**
+ * Primary UI component for user interaction
+ */
+export interface ButtonProps {
+    primary?: boolean,
+    backgroundColor?: string,
+    size?: string,
+    label: string,
+    onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+export const Button = (
+    {primary, backgroundColor, size, label, ...props}: ButtonProps,
+): JSX.Element => {
+    const mode = (primary === true)
+                 ? "storybook-button--primary"
+                 : "storybook-button--secondary" ;
+    return (
+        <button
+            type="button"
+            className={["storybook-button", `storybook-button--${String(size)}`, mode].join(" ")}
+            style={(backgroundColor != null)
+                   ? {backgroundColor}
+                   : {}}
+            {...props}
+        >
+            {label}
+        </button>
+    ) ;
+} ;
+
+// Button.propTypes = {
+//     /**
+//      * Is this the principal call to action on the page?
+//      */
+//     primary: PropTypes.bool,
+//     /**
+//      * What background color to use
+//      */
+//     backgroundColor: PropTypes.string,
+//     /**
+//      * How large should the button be?
+//      */
+//     size: PropTypes.oneOf(["small", "medium", "large"]),
+//     /**
+//      * Button contents
+//      */
+//     label: PropTypes.string.isRequired,
+//     /**
+//      * Optional click handler
+//      */
+//     onClick: PropTypes.func,
+// } ;
+
+Button.defaultProps = {
+    backgroundColor: undefined,
+    primary        : false,
+    size           : "medium",
+    onClick        : undefined,
+} ;
